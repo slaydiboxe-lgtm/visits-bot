9269d01793b3671a31bd01d64cd7e89ebe2f2064
slaydiboxe-lgtm <slaydiboxe@gmail.com>
2026-05-18 16:38:21 +0545

    Use environment variable for bot token

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import aiohttp
 import os
 
-TOKEN = "YOUR_BOT_TOKEN"
+TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
